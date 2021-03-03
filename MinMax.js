e77dccd52f700fe0f7b474fb4b5cf547a7ff3b95
akshay.dhawle@neosofttech.com <akshay.dhawle@neosofttech.com>
2021-03-03 17:56:11 +0530

    initial commit

---
+++ MinMax.js
@@ -0,0 +1,27 @@
+let arr = [5, 2, 7, 9, 13, 3, 8];
+
+function MinMaxNumber(arr) {
+	// initialize values
+	let min = arr[0],
+		max = arr[0];
+	let minIndex = 0,
+		maxIndex = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		// take min , max initiaal values before change
+		let v = arr[i],
+			minValue = min,
+			maxValue = max;
+
+		min = v < min ? v : min;
+		max = v > max ? v : max;
+
+		// after change min , max values so we will get index and we can update
+		if (minValue !== min) minIndex = i;
+		if (maxValue !== max) maxIndex = i;
+	}
+
+	return maxIndex - minIndex;
+}
+
+console.log(MinMaxNumber(arr));
